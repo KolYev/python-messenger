1ed193c136e688d59e59683ea36b4a7eacee3b64
KolYev <evgeniy.kolesenko@gmail.com>
2026-04-18 21:36:22 +0300

    узнаём публичный IP адрес

--- p2p.py
+++ p2p.py
@@ -1,4 +1,5 @@
 import socket
+from requests import get
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -6,5 +7,9 @@ sock.bind(('', 0))
 
 local_address, local_port = sock.getsockname()
 
+ip = get('https://api.ipify.org').content.decode('utf8')
+
+
 print(f"Ваш локальный UDP IP: {local_address}")
 print(f"Ваш локальный UDP порт: {local_port}")
+print('Ваш публичный IP адрес: {}'.format(ip))

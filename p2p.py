f46e173bea71f9a9d2a6d3599ae0db64b6735b5a
KolYev <evgeniy.kolesenko@gmail.com>
2026-04-18 22:00:37 +0300

    получаем информацию о публичном порте и типе NAT

--- p2p.py
+++ p2p.py
@@ -1,15 +1,24 @@
 import socket
-from requests import get
+import stun
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def get_local_info():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', 0))
 
-sock.bind(('', 0))
+    local_address, local_port = sock.getsockname()
+    
+    print(f"Ваш локальный UDP IP: {local_address}")
+    print(f"Ваш локальный UDP порт: {local_port}")
+    
+def get_external_info():
+    nat_type, external_ip, external_port = stun.get_ip_info(
+        stun_host='stun.l.google.com', 
+        stun_port=19302
+    )
 
-local_address, local_port = sock.getsockname()
-
-ip = get('https://api.ipify.org').content.decode('utf8')
-
-
-print(f"Ваш локальный UDP IP: {local_address}")
-print(f"Ваш локальный UDP порт: {local_port}")
-print('Ваш публичный IP адрес: {}'.format(ip))
+    
+    print(f"Ваш публичный IP адрес: {external_ip}")
+    print(f"Ваш публичный UDP порт: {external_port}")
+    print(f"Ваш тип NAT: {nat_type}")
+    
+get_external_info()
